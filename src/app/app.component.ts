d28c19bf7f0a34cc2f9a6590a986bdaacbfe8a5b
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-03-11 18:31:56 +0530

    will try to renderd the components and and routing system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componants/header/header.component';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
     <app-header></app-header>
-    <app-home></app-home>
-    <router-outlet></router-outlet>
+    <router-outlet></router-outlet> <!-- Router handles which component to show -->
   `,
 })
 export class AppComponent {}
