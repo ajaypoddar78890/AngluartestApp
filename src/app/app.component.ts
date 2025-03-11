85491d3f9c824071655cfbb8d7dc50b55c9eb0f0
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-03-11 13:13:05 +0530

    added the 2 more component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './componants/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  template: `
+    <app-header></app-header>
+    <app-home></app-home>
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent {
-  title = 'angtestapp';
-}
+export class AppComponent {}
