174cec0022c21e041bbb591575a86726cacf44b0
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-03-12 15:40:37 +0530

    created the counter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { GreeterComponent } from '../componants/greeter/greeter.component';
+import { CounterComponent } from '../componants/counter/counter.component';
 
 @Component({
   selector: 'app-home',
-  imports: [GreeterComponent],
+  imports: [GreeterComponent, CounterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  keyupHandler(event: KeyboardEvent) {
-    console.log(`user typed something ${event.key} `);
+  counter: number = 0;
+
+  increaseCounter() {
+    this.counter++;
+  }
+
+  decreaseCounter() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
   }
 }
+
+// export class HomeComponent {
+//   counter: number = 0; // ✅ Properly declared property
+
+//   increaseCounter() {
+//     this.counter++; // ✅ Increment counter
+//   }
+
+//   decreaseCounter() {
+//     if (this.counter > 0) {
+//       this.counter--; // ✅ Decrement counter
+//     }
+//   }
+// }
