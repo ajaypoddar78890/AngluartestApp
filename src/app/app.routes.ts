1b6d0abed33017f4c9fb54d2d09c43a3f6beb94e
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2025-03-12 17:17:23 +0530

    finieshed the routing works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,9 @@ import { AboutComponent } from './about/about.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'about', component: AboutComponent }, // About page route
+  {
+    path: 'todos',
+    loadComponent: () =>
+      import('./todos/todos.component').then((m) => m.TodosComponent),
+  },
 ];
